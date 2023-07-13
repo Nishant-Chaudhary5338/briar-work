d998e2c85e41db7459365a3042633e2df47743cd
Nishant Chaudhary <nishantchaudhary5338@gmail.com>
2023-07-13 12:18:51 +0530

    reverted previous test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HeroSection from "./pages/HeroSection";
 import HomePage from "./pages/HomePage";
-import Tesr from "./components/Tesr";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Tesr />} />
+      <Route path='/' element={<LoginPage />} />
       <Route path='/hero' element={<HeroSection />} />
       <Route path='/home' element={<HomePage />} />
     </Routes>
